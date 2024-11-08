ecf9b7352cb039d5daf09a2dd96033386d41972d
brakhimb <rahimbaevbekzhan@gmail.com>
2024-11-08 17:07:54 +0500

    for updating

--- src/Observer/Waiter.java
+++
@@ -1,9 +0,0 @@
-package Observer;
-
-public class Waiter implements Observer {
-    private String name;
-    public Waiter(String name) { this.name = name; }
-    public void update(String message) {
-        System.out.println(name + " received notification: " + message);
-    }
-}
